54204149d61b6e965a6ed9aaf774b909c42e106f
saikatxshrey <saikatxshrey@gmail.com>
2022-04-29 21:49:28 +0530

    added basic route

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-// dotenv
+// packages
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 
+// database
 const connectDB = require("./db/connect");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ecommerce");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 // server start
 const port = process.env.PORT || 5000;
